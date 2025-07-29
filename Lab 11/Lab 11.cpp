1c9f373924a9d31158ce237761f979664772aaae
Dr. Alsayed <Dr. Alsayed@DESKTOP-APIOASQ>
2025-07-29 21:07:48 +0300

    Add project files.

---
+++ Lab 11/Lab 11.cpp
@@ -0,0 +1,26 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+   
+	int num1;
+
+	cout << "Please, enter a number: ";
+	cin >> num1;
+
+	if (num1 > 0) {
+		cout << num1 << " is a positive number "<<"\n";
+	}
+
+
+
+
+
+
+
+	return 0;
+}
+
+
